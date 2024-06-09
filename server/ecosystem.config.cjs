29ca18e1582289222670b78bf6703b2af7918817
DLaptiev <akiosarkiz@gmail.com>
2024-06-09 19:22:06 +0200

    update ecosystem

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -4,18 +4,24 @@ module.exports = {
    */
   apps: [
     {
-      name: "mangaland",
-      script: "dist/index.js",
+      name: 'mangaland',
+      script: 'dist/index.js',
 
       listen_timeout: 3_000,
       kill_timeout: 10_000,
       wait_ready: true,
       instances: 2,
-      exec_mode: "cluster",
+      exec_mode: 'cluster',
 
       env: {
-        NODE_ENV: "production",
+        NODE_ENV: 'production',
       },
+    },
+    {
+      name: 'mangaland-scrape',
+      script: 'dist/src/cron/scrape.js',
+      instances: 1,
+      cron_restart: '0 * * * *',
     },
   ],
 };
